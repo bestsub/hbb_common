11557a8a2316fc66481521b5ec53026033ed94a0
bestsub <webdeveloper@bestsub.net>
2025-06-03 15:26:06 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["gzbsdesk.best-sub.com"];
+pub const RS_PUB_KEY: &str = "p81yKMu9X+JRLmCg+Zhh1vbeE4gBU8PkrAt+jJqlpEM=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
